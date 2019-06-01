c67a45a7e1269b4ff76e6774ff4bc7da31697b59
pyc-ycy <2923616405@qq.com>
2019-06-01 20:08:59 +0800

    Update

---
+++ src/Java常用类/test.java
@@ -0,0 +1,12 @@
+package Java常用类;//Java常用类
+
+//彭友聪
+//2019/6/1
+//19:59
+//IDEA_JAVA
+//email:2923616405@qq.com
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
