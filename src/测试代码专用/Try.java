7bf3909ec0b3b740e5b6b210b49dc543bba60bc1
御承扬 <2923616405@qq.com>
2019-06-23 15:40:20 +0800

    Update

---
+++ src/测试代码专用/Try.java
@@ -0,0 +1,20 @@
+package 测试代码专用;
+
+// !/usr/bin/env jdk1.8
+// encoding:utf-8
+//@software:IntelliJ IDEA
+//@pack:测试代码专用
+//@user:彭友聪
+//@date:2019/6/22
+//@time:10:35
+//@project:IDEA_JAVA
+//@file:Try.java
+//Author:御承扬
+//email:2923616405@qq.com
+
+public class Try {
+    public static void main(String[] args) {
+        int Thisisnumber = 15;
+        System.out.println(Thisisnumber);
+    }
+}
