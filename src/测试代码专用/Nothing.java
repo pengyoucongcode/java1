83b6817bf71d6665fa77819db284de9ddd905cc8
御承扬 <2923616405@qq.com>
2019-08-13 17:01:46 +0800

    Update

---
+++ src/测试代码专用/Nothing.java
@@ -0,0 +1,21 @@
+// >= JDK 1.8
+// encoding:utf-8
+// @software:IntelliJ IDEA
+// @user:彭友聪
+// @date:2019/08/13
+// @time:上午 8:51
+// @project:IDEA_JAVA
+// @file:Nothing.java
+// @Author:御承扬
+//@E-mail:2923616405@qq.com
+
+package 测试代码专用;
+
+public class Nothing {
+    public Nothing() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println( "Hello world" );
+    }
+}
